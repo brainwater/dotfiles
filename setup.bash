4e0ad11cf5fddfb6027aa7d76327deebad273339
Blake Rainwater <rainwaterblake@gmail.com>
2015-03-22 21:20:36 -0400

    Made multiple runs of setup.bash idempotent

--- setup.bash
+++ setup.bash
@@ -1,10 +1,10 @@
 cd "${HOME}"
-ln -s ../dotfiles/gtk-2.0 .config/gtk-2.0
-ln -s ../dotfiles/gtk-3.0 .config/gtk-3.0
-ln -s ../dotfiles/openbox .config/openbox
-ln -s ../dotfiles/tint2 .config/tint2
+ln -n -s ../dotfiles/gtk-2.0 .config/gtk-2.0
+ln -n -s ../dotfiles/gtk-3.0 .config/gtk-3.0
+ln -n -s ../dotfiles/openbox .config/openbox
+ln -n -s ../dotfiles/tint2 .config/tint2
 mkdir .emacs.d
-ln -s ../dotfiles/emacs/init.el .emacs.d/init.el
-ln -s ../dotfiles/emacs/elisp .emacs.d/elisp
-ln -s dotfiles/bash/bash_profile .bash_profile
-ln -s dotfiles/xmodmap/Xmodmap .Xmodmap
+ln -n -s ../dotfiles/emacs/init.el .emacs.d/init.el
+ln -n -s ../dotfiles/emacs/elisp .emacs.d/elisp
+ln -n -s dotfiles/bash/bash_profile .bash_profile
+ln -n -s dotfiles/xmodmap/Xmodmap .Xmodmap
