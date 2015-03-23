18bcd6416c19838ea5f7a2ddcb74028f9760a9bc
Blake Rainwater <rainwaterblake@gmail.com>
2015-03-22 21:00:17 -0400

    Fixed error with setup script

--- setup.bash
+++ setup.bash
@@ -2,7 +2,7 @@ cd "${HOME}"
 ln -s ../dotfiles/gtk-2.0 .config/gtk-2.0
 ln -s ../dotfiles/gtk-3.0 .config/gtk-3.0
 ln -s ../dotfiles/openbox .config/openbox
-ln -s ../dotfiles/tint2
+ln -s ../dotfiles/tint2 .config/tint2
 mkdir .emacs.d
 ln -s ../dotfiles/emacs/init.el .emacs.d/init.el
 ln -s ../dotfiles/emacs/elisp .emacs.d/elisp
