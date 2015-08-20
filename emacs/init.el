5aefe47f1dc47f0907852e54b1523661de2136ff
Blake Rainwater <rainwaterblake@gmail.com>
2015-08-20 18:47:03 -0400

    Use dark theme

--- emacs/init.el
+++ emacs/init.el
@@ -29,3 +29,15 @@
 
 (setq-default c-default-style "linux"
 	      c-basic-offset 4)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (manoj-dark))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
