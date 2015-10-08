d2da9e0059ad9eb50a617b685dc1526d6e81758b
Blake Rainwater <rainwaterblake@gmail.com>
2015-10-08 14:46:24 -0400

    Add script for doing configurations that require root priviledges and a xsession entry for locally compiled openbox

---
+++ rootconfig/rootsetup.bash
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Copy the openbox desktop session file that will run openbox from /usr/local
+sudo cp -n -v "${HOME}/dotfiles/rootconfig/xsessions/CustomOpenbox.desktop" "/usr/share/xsessions/CustomOpenbox.desktop"
