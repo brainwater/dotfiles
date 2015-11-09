7ece84a869f22afb15a8b8b2d5775e595dc425a9
Blake Rainwater <rainwaterblake@gmail.com>
2015-11-09 16:28:55 -0500

    Add Gtags for c and c++ source code navigation

--- emacs/init.el
+++ emacs/init.el
@@ -25,6 +25,7 @@
     fill-column-indicator
     python-mode
     pymacs
+    ggtags
     ) "List of packages to ensure are installed at launch." )
 
 (require 'my-packages)
@@ -57,6 +58,20 @@
 (add-hook 'python-mode-hook 'fci-mode)
 (remove-hook 'python-mode-hook 'pylint-add-key-bindings)
 (remove-hook 'python-mode-hook 'pylint-add-menu-items)
+
+(require 'ggtags)
+(add-hook 'c-mode-common-hook
+          (lambda ()
+            (when (derived-mode-p 'c-mode 'c++-mode 'java-mode 'asm-mode)
+              (ggtags-mode 1))))
+
+(define-key ggtags-mode-map (kbd "C-c g s") 'ggtags-find-other-symbol)
+(define-key ggtags-mode-map (kbd "C-c g h") 'ggtags-view-tag-history)
+(define-key ggtags-mode-map (kbd "C-c g r") 'ggtags-find-reference)
+(define-key ggtags-mode-map (kbd "C-c g d") 'ggtags-find-definition)
+(define-key ggtags-mode-map (kbd "C-c g f") 'ggtags-find-file)
+(define-key ggtags-mode-map (kbd "C-c g c") 'ggtags-create-tags)
+(define-key ggtags-mode-map (kbd "C-c g u") 'ggtags-update-tags)
 
 (setq-default c-default-style "linux"
 	      c-basic-offset 4)
