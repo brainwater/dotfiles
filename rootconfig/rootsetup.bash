792fecbd0dbb66b64548aa7f53012c5173ca90ff
Blake Rainwater <rainwaterblake@gmail.com>
2016-01-27 16:13:06 -0500

    Add install for gmrun

--- rootconfig/rootsetup.bash
+++ rootconfig/rootsetup.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Install packages we need
-sudo apt-get install -y feh tint2 emacs global autossh
+sudo apt-get install -y feh tint2 emacs global autossh gmrun
 # Copy the openbox desktop session file that will run openbox from /usr/local
 sudo cp -n -v "${HOME}/dotfiles/rootconfig/xsessions/CustomOpenbox.desktop" "/usr/share/xsessions/CustomOpenbox.desktop"
