92296b38fd09961dfcb1735954b7c2c06300c917
Blake Rainwater <rainwaterblake@gmail.com>
2016-08-24 14:29:23 -0400

    Add script for notification

--- rootconfig/rootsetup.bash
+++ rootconfig/rootsetup.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Install packages we need
-sudo apt-get install -y feh tint2 emacs global autossh gmrun gnome-terminal volumeicon-alsa
+sudo apt-get install -y feh tint2 emacs global autossh gmrun gnome-terminal volumeicon-alsa curl
 # Copy the openbox desktop session file that will run openbox from /usr/local
 sudo cp -n -v "${HOME}/dotfiles/rootconfig/xsessions/CustomOpenbox.desktop" "/usr/share/xsessions/CustomOpenbox.desktop"
--- setup.bash
+++ setup.bash
@@ -15,3 +15,4 @@ mkdir bin
 ln -n -s ../dotfiles/dotfiles-private/scripts/tunnel-listen bin/tunnel-listen
 ln -n -s ../dotfiles/dotfiles-private/scripts/tunnel-connect bin/tunnel-connect
 ln -n -s ../dotfiles/openbox/fixscreen.bash bin/fixscreen.bash
+ln -n -s ../dotfiles/dotfiles-private/scripts/textme bin/textme
