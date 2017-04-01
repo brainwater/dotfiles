7859455a84f40407a93a0bf7052c3258c4a30ad9
Blake Rainwater <rainwaterblake@gmail.com>
2017-04-01 02:21:38 -0400

    Improved emacs python

--- emacs/init.el
+++ emacs/init.el
@@ -2,9 +2,16 @@
 
 (require 'package)
 ;; Add marmalade, melpa, and org repositories
-(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
-			 ("melpa" . "https://melpa.milkbox.net/packages/")
-			 ("marmalade" . "https://marmalade-repo.org/packages/")))
+;;(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages"))
+;;(add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/"))
+;;(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages"))
+;;(setq package-archives '(("melpa" . "https://melpa.org/packages/")))
+			 ;;("gnu" . "https://elpa.gnu.org/packages/")
+			 ;;("melpa" . "https://melpa.milkbox.net/packages/")
+                         
+                          ;;("marmalade" . "https://marmalade-repo.org/packages/")))
+			 ;;)
 
 ;; Found at http://www.emacswiki.org/emacs/ShowParenMode
 (show-paren-mode 1)
@@ -21,10 +28,8 @@
     slime
     haskell-mode
     auto-complete
-    ac-python
     fill-column-indicator
-    python-mode
-    pymacs
+    elpy
     ggtags
     ) "List of packages to ensure are installed at launch." )
 
@@ -34,30 +39,21 @@
 
 (global-linum-mode t)
 
+
 ;;;(require 'ido)
 ;;;(ido-mode t)
 
-(require 'python-mode)
-(setq-default py-shell-name "ipython3")
-(setq-default py-which-bufname "ipython3")
-(setq py-python-command-args
-      '("--gui=wx" "--pylab=wx" "-colors" "Linux"))
-(setq py-force-py-shell-name-p t)
-(setq py-shell-switch-buffers-on-execute-p t)
-(setq py-switch-buffers-on-execute-p t)
-(setq py-split-windows-on-execute-p nil)
-(setq py-smart-indentation t)
 
 (require 'auto-complete)
 (require 'auto-complete-config)
-(require 'ac-python)
-(ac-config-default)
+
+(elpy-enable)
+(pyvenv-activate (substitute-in-file-name "$HOME/anaconda3"))
+
 
 (require 'fill-column-indicator)
 (add-hook 'c-mode-hook 'fci-mode)
 (add-hook 'python-mode-hook 'fci-mode)
-(remove-hook 'python-mode-hook 'pylint-add-key-bindings)
-(remove-hook 'python-mode-hook 'pylint-add-menu-items)
 
 (require 'ggtags)
 (add-hook 'c-mode-common-hook
